418eb82bfcffe7f685aec507716478280b80305b
Darshan Madhu <97115194+darshanmadhu@users.noreply.github.com>
2022-05-26 15:56:57 +0000

    error with problem12

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -1,2 +1,9 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+ 
+import re     
+hand = open("regex_sum_340267.txt", "r")
+numlist = []      
+for line in hand:
+    line = line.rstrip()
+    integers = re.findall('([0-9]+)', line)   
+    for number in integers: numlist.append(int(number))            an
+                                 
