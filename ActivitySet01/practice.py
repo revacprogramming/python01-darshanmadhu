dd9740e3e59771e678f25a6845ebbe83f479e023
Darshan Madhu <97115194+darshanmadhu@users.noreply.github.com>
2022-05-30 15:23:46 +0000

    Problem

--- ActivitySet01/practice.py
+++ ActivitySet01/practice.py
@@ -1,5 +1,2 @@
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("hi hello , i am python module , welcome go the world of python")
-engine.runAndWait()
-#text to speech python module
+input("")
+print(input(" ")
